5a12c3c8b82d2e549ef77fe199177cdaafe1385a
yash <sakariyayash05@gamil.com>
2023-10-18 10:31:50 +0530

    test

---
+++ lib/geolocater/geolocater_screen.dart
@@ -0,0 +1,13 @@
+class GeolocaterPage extends StatefulWidget {
+  const GeolocaterPage({super.key});
+
+  @override
+  State<GeolocaterPage> createState() => _GeolocaterPageState();
+}
+
+class _GeolocaterPageState extends State<GeolocaterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
